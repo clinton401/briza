b7700306f38ba6ad3142a1d2468fa85cbefcd1ff
clinton <clintonphillips464@gmail.com>
2025-04-19 19:50:43 +0100

    added metadata for compose page

--- app/compose/page.tsx
+++ app/compose/page.tsx
@@ -1,12 +1,16 @@
-import {FC} from "react";
-import {CreatePostUI} from "@/components/post/create-post-ui";
-import getServerUser from "@/hooks/get-server-user"
+import { FC } from "react";
+import { CreatePostUI } from "@/components/post/create-post-ui";
+import getServerUser from "@/hooks/get-server-user";
 
+export const metadata = {
+  title: 'Compose Post',
+  description: 'Share your thoughts with the world by composing a new post on Briza.',
+};
 
-const ComposePage: FC = async() => {
-    const session = await getServerUser();
-    if (!session) return;
-    return <CreatePostUI session={session}/>
-}
+const ComposePage: FC = async () => {
+  const session = await getServerUser();
+  if (!session) return;
+  return <CreatePostUI session={session} />;
+};
 
 export default ComposePage;
